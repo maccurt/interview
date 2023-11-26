b70b0100796c0568e4f262c4f2adf0b9e64e141f
Michael McGee <dineromike@yahoo.com>
2023-11-26 08:35:38 -0600

    made change to commit

--- src/app/poker.service.isStraight.spec.ts
+++ src/app/poker.service.isStraight.spec.ts
@@ -9,7 +9,7 @@ describe('isStraight', () => {
         service = TestBed.inject(PokerService);
     });
 
-    describe('check all errors', () => {
+    describe('check all error s', () => {
 
         describe('playerMustHave2Cards: a player must have 2 cards in their hand', () => {
 
